76b1b5520a614dfcc5cc05596eea4d30118051c7
Luu Minh <minhkhmt1k3@gmail.com>
2018-05-08 11:21:44 +0700

    update description

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">ReactJS - Deploy to github</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Let's start your project :)
         </p>
       </div>
     );
